debb5d374bb6c43bc7e52b294927378cba350a8e
Mikaila Brown <mbr2201@wgu.edu>
2024-04-16 10:45:01 -0400

    add api folder

--- server/api/server.js
+++ server/api/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import records from "./routes/record.js";
+import records from "../routes/record.js";
 import dotenv from 'dotenv';
 
 dotenv.config();
