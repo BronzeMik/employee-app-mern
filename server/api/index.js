08bc018bfa440973f5bbf5041dbe9b1c44f805ec
Mikaila Brown <mbr2201@wgu.edu>
2024-04-16 11:25:28 -0400

    update cors

--- server/api/index.js
+++ server/api/index.js
@@ -8,7 +8,11 @@ dotenv.config();
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://employee-app-mern.vercel.app/"],
+  methods: ["POST", "GET", "PATCH", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 app.use("/record", records);
 
