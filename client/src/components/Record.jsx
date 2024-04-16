1ee02d217fee1d4d4ae7d3668c173c08e6683909
Mikaila Brown <mbr2201@wgu.edu>
2024-04-16 11:26:19 -0400

    update client api call

--- client/src/components/Record.jsx
+++ client/src/components/Record.jsx
@@ -17,7 +17,7 @@ export default function Record() {
       if(!id) return;
       setIsNew(false);
       const response = await fetch(
-        `http://employee-app-mern-api.vercel.app/record/${params.id.toString()}`
+        `https://employee-app-mern-api.vercel.app/record/${params.id.toString()}`
       );
       if (!response.ok) {
         const message = `An error has occurred: ${response.statusText}`;
@@ -51,7 +51,7 @@ export default function Record() {
       let response;
       if (isNew) {
         // if we are adding a new record we will POST to /record.
-        response = await fetch("http://employee-app-mern-api.vercel.app/record", {
+        response = await fetch("https://employee-app-mern-api.vercel.app/record", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -60,7 +60,7 @@ export default function Record() {
         });
       } else {
         // if we are updating a record we will PATCH to /record/:id.
-        response = await fetch(`http://employee-app-mern-api.vercel.app/record/${params.id}`, {
+        response = await fetch(`https://employee-app-mern-api.vercel.app/record/${params.id}`, {
           method: "PATCH",
           headers: {
             "Content-Type": "application/json",
