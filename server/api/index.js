498c08f3deae56bc4754d81f78c250d284a0a844
Mikaila Brown <mbr2201@wgu.edu>
2024-04-16 20:53:59 -0400

    update vercel json headers

--- server/api/index.js
+++ server/api/index.js
@@ -8,25 +8,13 @@ dotenv.config();
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://employee-app-mern.vercel.app");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE"
-  );
-  res.header("Access-Control-Allow-Credentials",true);
-  next();
-});
-// app.use(cors({
-//   origin: ['*'],
-//   methods: ["POST", "GET", "PATCH", "PUT", "DELETE", "OPTIONS"],
-//   credentials: true,
-//   optionsSuccessStatus: 200
-// }));
+
+app.use(cors({
+  origin: ['*'],
+  methods: ["POST", "GET", "PATCH", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+  optionsSuccessStatus: 200
+}));
 app.use(express.json());
 app.use("/record", records);
 
