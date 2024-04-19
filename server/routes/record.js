ff4bdfc3c297b9583d17b2d229a132b9db8b3e2a
Mikaila Brown <mbr2201@wgu.edu>
2024-04-19 09:33:12 -0400

    update routes

--- server/routes/record.js
+++ server/routes/record.js
@@ -14,17 +14,28 @@ const router = express.Router();
 
 // This section will help you get a list of all the records.
 router.get("/", async (req, res) => {
+  if(req.method === 'OPTIONS') {
+    return res.status(200).json(({
+        body: "OK"
+    }))
+}
   let collection = await db.collection("records");
   let results = await collection.find({}).toArray();
+  
   res.send(results).status(200);
 });
 
 // This section will help you get a single record by id
 router.get("/:id", async (req, res) => {
+  if(req.method === 'OPTIONS') {
+    return res.status(200).json(({
+        body: "OK"
+    }))
+}
   let collection = await db.collection("records");
   let query = { _id: new ObjectId(req.params.id) };
   let result = await collection.findOne(query);
-
+  
   if (!result) res.send("Not found").status(404);
   else res.send(result).status(200);
 });
@@ -32,6 +43,11 @@ router.get("/:id", async (req, res) => {
 // This section will help you create a new record.
 router.post("/", async (req, res) => {
   try {
+    if(req.method === 'OPTIONS') {
+      return res.status(200).json(({
+          body: "OK"
+      }))
+  }
     let newDocument = {
       name: req.body.name,
       position: req.body.position,
@@ -48,6 +64,11 @@ router.post("/", async (req, res) => {
 
 // This section will help you update a record by id.
 router.patch("/:id", async (req, res) => {
+  if(req.method === 'OPTIONS') {
+    return res.status(200).json(({
+        body: "OK"
+    }))
+}
   try {
     const query = { _id: new ObjectId(req.params.id) };
     const updates = {
@@ -69,6 +90,11 @@ router.patch("/:id", async (req, res) => {
 
 // This section will help you delete a record
 router.delete("/:id", async (req, res) => {
+  if(req.method === 'OPTIONS') {
+    return res.status(200).json(({
+        body: "OK"
+    }))
+}
   try {
     const query = { _id: new ObjectId(req.params.id) };
 
